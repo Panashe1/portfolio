08477072413310579d0f250ea04aa07611ff31ee
Panashe Mugayapi <pmugayapi@hotmail.com>
2025-02-24 00:41:09 +1100

    Font changes + changed a link error

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -93,7 +93,7 @@ const Contact = () => {
         whileHover={{ scale: 1.05 }} 
         transition={{ duration: 0.3 }}
         type='submit'
-        className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
+        className='font-Handjet py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'
         >Submit now <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.button>
 
         <p className='mt-4'>{result}</p>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,18 +8,18 @@ const Footer = ({isDarkMode}) => {
       <div className='text-center'>
         <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
 
-        <div className='w-max flex items-center gap-2 mx-auto'>
+        <div className='font-Handjet w-max flex items-center gap-2 mx-auto'>
             <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
             pmugayapi@hotmail.com
         </div>
       </div>
 
-    <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 Panashe Mugayapi. All rights reserved.</p>
+    <div className='font-Handjet text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
+        <p className='font-Handjet'>© 2025 Panashe Mugayapi. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://github.com/Panashe1">GitHub</a></li>
-            <li><a target='_blank' href="www.linkedin.com/in/panashe-mugayapi-7a5861185">LinkedIn</a></li>
-            <li><a target='_blank' href="https://www.instagram.com/panashenathan">Instagram</a></li>
+            <li><a  className='font-Handjet'  target='_blank' href="https://github.com/Panashe1">GitHub</a></li>
+            <li><a  className='font-Handjet' target='_blank' href="https://linkedin.com/in/panashe-mugayapi-7a5861185">LinkedIn</a></li>
+            <li><a  className='font-Handjet' target='_blank' href="https://www.instagram.com/panashenathan">Instagram</a></li>
         </ul>
     </div>
 
