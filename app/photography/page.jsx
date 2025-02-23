688c11dca260ab5f39ad94ddba2b20caed4cba87
Panashe Mugayapi <pmugayapi@hotmail.com>
2025-02-24 02:40:27 +1100

    Added a new photography page
    Need to link the page and make it accessible. Need to fix the Navbar on the photography page.

---
+++ app/photography/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
+import Footer from "@/components/Footer";
+import PhotoGrid from "@/components/PhotoGrid";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "";
+    }
+  }, [isDarkMode]);
+
+  return (
+    <>
+      {/* <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} /> */}
+      <PhotoGrid isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
+    </>
+  );
+}
